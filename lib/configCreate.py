1e80ddd4e9236a061b6e6cb6a28508e5e982e980
Peter <10545710+peted-davis@users.noreply.github.com>
2019-06-20 10:44:58 +0100

    Update version number

--- lib/configCreate.py
+++ lib/configCreate.py
@@ -26,7 +26,7 @@ from pathlib import Path
 from geopy import distance as geopy
 
 # Define wfpiconsole version number
-Version = 'v2.8'
+Version = 'v2.9'
 
 # Define required variables
 stationWF = None
