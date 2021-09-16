56e9939e4b0d116e3a522121322ad7153c171eb9
Peter Davis <10545710+peted-davis@users.noreply.github.com>
2021-09-16 20:16:03 +0100

    Bug fix *Fixed missing logger in Weatherflow requestAPI

--- lib/requestAPI/weatherflow.py
+++ lib/requestAPI/weatherflow.py
@@ -16,6 +16,7 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 # Import required modules
+from kivy.logger import Logger
 from datetime    import datetime, timedelta
 from lib         import system
 import requests
